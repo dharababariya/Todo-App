9d6fff7c9fa7f4d19c86650314baf5834cac4a95
Dhara <dharababariya34@gmail.com>
2019-03-11 13:02:29 +0530

    remove tak

--- controller/todocontroller.js
+++ controller/todocontroller.js
@@ -1,6 +1,7 @@
 //add task
 
-const task = ["buy stock", "pratice with nodejs"]
+const task = ["buy stock", "pratice with nodejs"];
+const complete = ["finish jquery"];
 
 module.exports = function (app) {
 
@@ -16,19 +17,25 @@ module.exports = function (app) {
         task.push(newTask);
         res.redirect('/');
     });
-    const complete = ["finish jequry"]
-    app.post('/removetask', (req, res) => {
+    
+ //remove task
+    
+        app.post('/removetask', function (req, res) {
 
-        // const completeTask = req.body.check;
-        // if(typeof  completeTask  === "string"){
-        //     complete.push(completeTask);
-        //     task.splice(task.indexOf(completeTask), 1);
-        // } else if (typeof completeTask === "object") {
-        //     for (var i = 0; i < completeTask.length; i++) {     complete.push(completeTask[i]);
-        //     task.splice(task.indexOf(completeTask[i]), 1);
-        // }
-        // }
-        //    res.redirect("/");
-    })
+            
+            const completeTask = req.body.check;
 
-}
+            if (typeof completeTask === "string") {
+                complete.push(completeTask);
+
+                task.splice(task.indexOf(completeTask), 1);
+            } else if (typeof completeTask === "object") {
+                for (var i = 0; i < completeTask.length; i++) {
+                    complete.push(completeTask[i]);
+                    task.splice(task.indexOf(completeTask[i]), 1);
+                }
+            }
+            res.redirect("/");
+        })
+
+    }
