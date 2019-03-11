73869f4cfc336c8a0e0678e045096e527d09e2c0
Dhara <dharababariya34@gmail.com>
2019-03-11 12:27:33 +0530

    create addtask

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const todocontroller = require('./controller/todocontroller');
 const app = express();
 const port = 3030;
 
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 //set template engine
 
 app.set('view engine', 'ejs');
 
-
 //fire controller
 todocontroller(app);
 
-
 //listern port
 
-app.listen(port,() =>{ console.log('port 3030')});
+app.listen(port, () => {
+    console.log('port 3030')
+});
--- controller/todocontroller.js
+++ controller/todocontroller.js
@@ -1,18 +1,34 @@
-module.exports = function(app){
-   
-    app.get('/todo',(req,res)=>{
+//add task
 
-        res.render('todo');
+const task = ["buy stock", "pratice with nodejs"]
+
+module.exports = function (app) {
+
+    app.get('/', (req, res) => {
+
+        res.render('to-do', {task: task});
     });
 
-    
-    app.post('/todo',(req,res)=>{
+    //post route
+    app.post('/addtask', (req, res) => {
 
-        
+        const newTask = req.body.newtask;
+        task.push(newTask);
+        res.redirect('/');
     });
+    const complete = ["finish jequry"]
+    app.post('/removetask', (req, res) => {
 
-    app.delete('/todo',(req,res)=>{
-        
+        // const completeTask = req.body.check;
+        // if(typeof  completeTask  === "string"){
+        //     complete.push(completeTask);
+        //     task.splice(task.indexOf(completeTask), 1);
+        // } else if (typeof completeTask === "object") {
+        //     for (var i = 0; i < completeTask.length; i++) {     complete.push(completeTask[i]);
+        //     task.splice(task.indexOf(completeTask[i]), 1);
+        // }
+        // }
+        //    res.redirect("/");
     })
 
 }
